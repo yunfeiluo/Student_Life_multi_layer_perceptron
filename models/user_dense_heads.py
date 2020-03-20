db82cf1a30c3f18e65d2480f5e0e5b389f4ca70f
Yunfei Luo <33367743+yunfeiluo@users.noreply.github.com>
2020-03-20 16:17:58 -0400

    Add files via upload

--- models/user_dense_heads.py
+++ models/user_dense_heads.py
@@ -4,47 +4,6 @@ import torch.nn as nn
 
 from src.definitions import LOW_MODEL_CAPACITY_WARNING
 
-
-class UserDenseHead(nn.Module):
-    def __init__(self, users: list, input_size, hidden_size, num_classes, dropout=0, ordinal_regression_head=False):
-        """
-        This model has a dense layer for each student. This is used for MultiTask learning.
-
-        @param users: List of students (their ids) that are going to be used for trained.
-        The student ids much be strings.
-        @param input_size: Input size of each dense layer.
-        @param hidden_size: Hidden size of the dense layer.
-        """
-        super(UserDenseHead, self).__init__()
-        self.is_cuda_avail = True if torch.cuda.device_count() > 0 else False
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.num_classes = num_classes
-        self.dropout = dropout
-
-        # Layer initialization.
-        if self.input_size > self.hidden_size:
-            warnings.warn(LOW_MODEL_CAPACITY_WARNING)
-        dense_layer = {}
-        for user in users:
-            # todo(abhinavshaw): Make this configurable to any model of the users choice. can take those layers as a list.
-            sequential_liner = nn.Sequential(
-                nn.Linear(self.input_size, self.hidden_size),
-                nn.ReLU(),
-                # nn.Dropout(p=dropout),
-                nn.Linear(self.hidden_size, self.num_classes))
-
-            if ordinal_regression_head:
-                sequential_liner.add_module("sigmoid", nn.Sigmoid())
-
-            dense_layer[user] = sequential_liner
-
-        self.student_dense_layer = nn.ModuleDict(dense_layer)
-
-    def forward(self, user, input_data):
-        return self.student_dense_layer[user](input_data)
-
-
 class UserLSTM(nn.Module):
     def __init__(self, users: list,
                  input_size,
@@ -75,7 +34,7 @@ class UserLSTM(nn.Module):
         # Layer initialization.
         if self.input_size > self.lstm_hidden_size:
             warnings.warn(LOW_MODEL_CAPACITY_WARNING)
-        lstm_layer = dict()
+        lstm_layer = {}
         for user in users:
             # todo(abhinavshaw): Make this configurable to any model of the users choice. can take those layers as a list.
             lstm_layer[user] = nn.LSTM(input_size=input_size,
@@ -98,7 +57,7 @@ class UserLSTM(nn.Module):
 # Functionality: define MLP of each group of students
 # Author: Yunfei Luo
 # Start date: EST Feb.22th.2020
-# Last update: EST Feb.24th.2020
+# Last update: EST Mar.19th.2020
 # ----------------------------------------------------------------
 
 class GroupDenseHead(nn.Module):
