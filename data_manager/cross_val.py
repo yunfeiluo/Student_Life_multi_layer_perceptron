a5a3d2304d28770b7edeb445021bc1e621477950
Yunfei Luo <33367743+yunfeiluo@users.noreply.github.com>
2020-03-30 18:39:36 -0400

    Update cross_val.py

--- data_manager/cross_val.py
+++ data_manager/cross_val.py
@@ -17,13 +17,15 @@ SPLITTER_RANDOM_STATE = 100
 #   b) leave one student out split
 # Author: Yunfei Luo
 # Start date: EST Feb.22th.2020
-# Last update: EST Mar.20th.2020
+# Last update: EST Mar.30th.2020
 # ----------------------------------------------------------------
 
-def leave_one_subject_out_split(data: dict, groups:dict, subject='students'):
+def leave_one_subject_out_split(data: dict, groups: dict, ids: list, subject='students'):
     """
-
     @param data: data for which the splits are needed to be generated.
+    @param groups: dictionary, map: student_id -> group_id
+    @param ids: the list of id that want to be leaved out
+    @param subject: determine leave what subject out
     @return: Return list of dictionary (map: train_ids, val_ids -> data_keys)
     """
     print('########## leave one out split, subject: ' + subject + '##########')
@@ -49,39 +51,6 @@ def leave_one_subject_out_split(data: dict, groups:dict, subject='students'):
         splits.append(splitting_dict)
 
     return splits
-
-
-# def get_k_fod_cross_val_splits_stratified_by_students(data: dict, groups:dict, n_splits=5,
-#                                                       stratification_type="labels"):
-#     print('stratified by: ' + stratification_type)
-#     splits = list()
-
-#     data_keys = data['data'].keys()
-#     keys, labels = conversions.extract_keys_and_labels_from_dict(data)
-#     student_ids = conversions.extract_student_ids_from_keys(keys)
-#     student_ids_label = list()
-
-#     for i in range(len(student_ids)):
-#         student_ids_label.append(str(student_ids[i]) + "_" + str(labels[i]))
-
-#     student_ids_label = np.array(student_ids_label)
-#     data_keys = np.array(list(data_keys))
-#     student_ids = conversions.convert_list_of_strings_to_list_of_ints(student_ids)
-
-#     if stratification_type == "student":
-#         stratification_column = student_ids
-#     else:
-#         stratification_column = student_ids_label
-
-#     splitter = StratifiedKFold(n_splits=n_splits, random_state=SPLITTER_RANDOM_STATE)
-#     for train_index, val_index in splitter.split(X=data_keys, y=stratification_column):
-
-#         splitting_dict = dict()
-#         splitting_dict['train_ids'] = data_keys[train_index].tolist()
-#         splitting_dict['val_ids'] = data_keys[val_index].tolist()
-#         splits.append(splitting_dict)
-
-#     return splits
 
 def get_k_fod_cross_val_splits_stratified_by_students(data: dict, groups:dict, n_splits=5,
                                                       stratification_type="students"):
@@ -109,11 +78,20 @@ def get_k_fod_cross_val_splits_stratified_by_students(data: dict, groups:dict, n
         for key in data_keys:
             stratification_column.append(int(groups['student_' + key.split('_')[0]].split('_')[-1]))
     elif stratification_type == 'student_label':
+        # keys, labels = conversions.extract_keys_and_labels_from_dict(data)
+        # student_ids = conversions.extract_student_ids_from_keys(keys)
+
+        # for i in range(len(student_ids)):
+        #     stratification_column.append(str(student_ids[i]) + "_" + str(labels[i]))
+
         for key in data_keys:
             stratification_column.append(key.split('_')[0] + '_' + key.split('_')[-1])
     elif stratification_type == 'group_label':
         for key in data_keys:
             stratification_column.append(groups['student_' + key.split('_')[0]].split('_')[-1] + '_' + key.split('_')[-1])
+    else:
+        print('No such kind of criteria for splitting!!!')
+        exit()
 
     # splitting
     data_keys = np.array(list(data_keys))
