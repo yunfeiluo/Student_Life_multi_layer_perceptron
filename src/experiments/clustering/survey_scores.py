30415f7f70f5d2b9c6b9f2829c9013e20133dbcb
2630064129@qq.com <2630064129@qq.com>
2020-05-18 15:38:51 -0400

    update survey score clustering

--- src/experiments/clustering/survey_scores.py
+++ src/experiments/clustering/survey_scores.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import pickle
 
 # ---------------------------------------------------------------- 
 # Independent Study, Student Stree Prediction
@@ -181,9 +183,12 @@ def calc_panas():
     for i in range(5):
         pos_scale[i+1] = i+1
         neg_scale[i+1] = 0
-    pos_term = [1, 3, 5, 9, 10, 12, 14, 16, 17, 19]
+    # filter_q = [1, 3, 5, 9, 10, 12, 14, 16, 17, 19]
+    # pos_term = [1, 3, 5, 9, 10, 12, 14, 16, 17, 19]
+    filter_q = [2, 4, 6, 7, 8, 11, 13, 15, 18, 20]
+    pos_term = [2, 4, 6, 7, 8, 11, 13, 15, 18, 20]
     csv_file_path = "src/experiments/clustering/survey/panas.csv"
-    PSS_score = survey_scores(csv_file_path, neg_scale, pos_scale, pos_term, "pre_panas_positive", "post_panas_positive")
+    PSS_score = survey_scores(csv_file_path, neg_scale, pos_scale, pos_term, "pre_panas_negative", "post_panas_negative", filter_q)
 
 def calc_big_five():
     neg_scale = {"Disagree Strongly": 5, "Disagree a little": 4, "Neither agree nor disagree": 3, "Agree a little": 2, "Agree strongly": 1}
@@ -207,47 +212,88 @@ if __name__ == "__main__":
     # calc_lonliness_scale()
     # calc_flourishing_scale()
     # calc_panas()
-    calc_big_five()
+    # calc_big_five()
 
-    # # aggregate previous calculated scores (Big five, and some of the sleep score)
-    # df_out = pd.read_csv("src/experiments/clustering/survey/scores.csv", index_col="student_id")
-    # previous_df = pd.read_csv("Data/data/surveys_and_covariates/high_lelvel_aggregated_data.csv", index_col="student_id")
-    # new_col = dict() # map: col_name -> list()
-    # for item in previous_df.columns:
-    #     new_col[item] = list()
-    # # print(df_out.loc[0, 'post_PHQ_9'])
+    '''
+    group form: map: student_id -> group_id
+    '''
+    from sklearn.cluster import KMeans
+    student_list = [4, 7, 8, 10, 14, 16, 17, 19, 22, 23, 24, 32, 33, 35, 36, 43, 44, 49, 51, 52, 53, 57, 58]
+    df = pd.read_csv("src/experiments/clustering/survey/scores.csv", index_col="student_id")
+    student_survey_scores = dict()
+    missing_students = list()
+    for i, row in df.iterrows():
+        if i not in student_list:
+            continue
+        curr = list()
+        na = 0
+        for j in row:
+            if "n" in str(j): 
+                curr.append(-1)
+                na += 1
+            else:
+                curr.append(j)
+        student_survey_scores[i] = curr
+        if na > 0:
+            missing_students.append(i)
 
-    # # extract value from previous calculated file
-    # has = list()
-    # for i, row in df_out.iterrows():
-    #     has.append(i)
-    #     for item in new_col:
-    #         try:
-    #             new_col[item].append(previous_df.loc[i, item])
-    #         except:
-    #             new_col[item].append(None)
-    # for item in new_col:
-    #     df_out[item] = new_col[item]
+    print("missing students", missing_students)
+    ms = np.array([student_survey_scores[i] for i in student_survey_scores if i in missing_students])
+
+    num_groups = 2
+    # kmeans on clean dataset
+    X = np.array([student_survey_scores[i] for i in student_survey_scores if i not in missing_students])
+    kmeans = KMeans(n_clusters=num_groups, random_state=0).fit(X)
+
+    # store group info
+    student_group = dict() # map: student_id -> group
+    for i in student_list:
+        if i in missing_students:
+            continue
+        student_group[i] = kmeans.predict([np.array(student_survey_scores[i])])[0]
     
-    # new = list()
-    # for i, row in previous_df.iterrows():
-    #     if i not in has:
-    #         new.append(i)
-    # print('new', new)
+    # clustering the missing students, to the closest center
+    def clustering_missing_students(centers, missing_students, ms, student_group):
+        for i in range(len(ms)):
+            student_vec = [num for num in ms[i] if num >= 0]
+            close_ind = -1
+            close_dist = np.inf
+            for j in range(len(centers)):
+                center_vec = [centers[j][ind] for ind in range(len(ms[i])) if ms[i][ind] >= 0]
+                dist = np.linalg.norm(np.array(center_vec) - np.array(student_vec))
+                if dist < close_dist:
+                    close_dist = dist
+                    close_ind = j
 
-    # # append new row
-    # for id_ in new:
-    #     new_row = list()
-    #     for item in df_out.columns:
-    #         try:
-    #             new_row.append(previous_df.loc[id_, item])
-    #         except:
-    #             new_row.append(None)
-    #     df_out.loc[id_] = new_row
-    # print(df_out)
+            student_group[missing_students[i]] = close_ind
     
-    # # write to file
-    # df_out.to_csv('src/experiments/clustering/survey/scores.csv')
+    clustering_missing_students(kmeans.cluster_centers_, missing_students, ms, student_group)
+    
+    # check
+    group_student = dict() # map group -> list of student
+    for student in student_group:
+        try:
+            group_student[student_group[student]].append(student)
+        except:
+            group_student[student_group[student]] = [student]
+    for group in group_student:
+        print(group_student[group])
+    
+    # formalize
+    output_group = dict() # copy student_group with correct form
+    for i in student_group:
+        output_group['student_{}'.format(i)] = "group_{}".format(student_group[i])
+    
+    # write to file
+    with open("Data/student_groups/survey_scores_{}.pkl".format(num_groups), 'wb') as f:
+        pickle.dump(output_group, f)
+    
+    # check
+    with open("Data/student_groups/survey_scores_{}.pkl".format(num_groups), 'rb') as f:
+        readed_file = pickle.load(f)
+        for stu in readed_file:
+            print("{}, {}".format(stu, readed_file[stu]))
+
     
     # check missing data
     # missing_ids = list()
